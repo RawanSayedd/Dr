a0d3cb50c999d804811840be9fd7223d4323f739
RawanSayedd <rawansayed143@gmail.com>
2024-05-20 21:48:11 +0300

    for images and sounds

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
 }
-
 function handleClick() {
   makeSound(this.innerHTML);
   buttonAnimation(this.innerHTML);
